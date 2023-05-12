c6631ae135fbc4771ee19ae33b5f8316ddf7a9df
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-05-12 00:04:34 -0400

    Chore: Deleted un used files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function Faith () {
+    return (
+        <>
+        <h1> We have Faith </h1>
+        </>
+    );
+}
+
+ReactDom.render(<Faith />, document.getElementById('root'))
